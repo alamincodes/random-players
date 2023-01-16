163e8b4bbfcd0367ec66a888eca6571aea1cff06
Md Alamin <mdalamin88248@gmail.com>
2023-01-16 23:12:41 +0600

    change input style

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 document.getElementById("add-player").addEventListener("click", function () {
   const playerName = document.getElementById("player");
   const playerNameValue = playerName.value;
+  if (playerNameValue == "") {
+    return;
+  }
   const li = document.createElement("li");
   const players = document.getElementById("players");
   li.innerText = playerNameValue;
   players.appendChild(li);
   playerName.value = "";
-
 });
-
